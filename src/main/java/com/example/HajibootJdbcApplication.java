38116efefb9341a74697e4db44486be1173f10a2
Koji Sudo <higedrum.coz@gmail.com>
2016-05-04 21:40:36 +0900

    edit jdbc settings

--- src/main/java/com/example/HajibootJdbcApplication.java
+++ src/main/java/com/example/HajibootJdbcApplication.java
@@ -1,12 +1,34 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class HajibootJdbcApplication {
+public class HajibootJdbcApplication implements CommandLineRunner {
+
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		String sql = "SELECT 1";
+		SqlParameterSource param = new MapSqlParameterSource();
+		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+
+		System.out.println("result = " + result);
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HajibootJdbcApplication.class, args);
 	}
+
 }
