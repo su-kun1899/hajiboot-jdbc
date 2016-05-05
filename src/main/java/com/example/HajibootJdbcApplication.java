560fec2747953b63fb94b68d18ee30321956b724
Koji Sudo <higedrum.coz@gmail.com>
2016-05-05 19:45:04 +0900

    sql log settings

--- src/main/java/com/example/HajibootJdbcApplication.java
+++ src/main/java/com/example/HajibootJdbcApplication.java
@@ -16,7 +16,8 @@ import org.springframework.stereotype.Component;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class HajibootJdbcApplication implements CommandLineRunner {
 
     @Autowired
